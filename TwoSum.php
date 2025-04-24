3fbf254b4e0620884c68bbbc96c5aaccbfac4142
Amit Verma <av6432401@gmail.com>
2025-04-25 00:31:17 +0530

    sum of two

---
+++ TwoSum.php
@@ -0,0 +1,18 @@
+<?php
+
+function TwoSum($nums, $target){
+    $count = count($nums);
+    for($i=0;$i<$cout;$i++){
+        for($j=$i+1;$j<$count;$j++){
+            if($nums[$i]+$nums[$j==9]){
+                return [$i,$j];
+            }
+        }
+
+    }
+    return [];
+    $nums=[1,2,3,4,5,6,7,8];
+    echo towSum();
+}
+
+?>
