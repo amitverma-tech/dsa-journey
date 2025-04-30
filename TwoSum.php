1ba856ae67935faf2b9ae0eea2be6067d06f3ecb
Amit Verma <av6432401@gmail.com>
2025-04-30 17:36:30 +0530

    twoSum

--- TwoSum.php
+++ TwoSum.php
@@ -2,17 +2,21 @@
 
 function TwoSum($nums, $target){
     $count = count($nums);
-    for($i=0;$i<$cout;$i++){
-        for($j=$i+1;$j<$count;$j++){
-            if($nums[$i]+$nums[$j==9]){
-                return [$i,$j];
+    for($i = 0; $i < $count; $i++){
+        for($j = $i + 1; $j < $count; $j++){
+            if($nums[$i] + $nums[$j] == $target){
+                return [$i, $j];
             }
         }
-
     }
     return [];
-    $nums=[1,2,3,4,5,6,7,8];
-    echo towSum();
 }
 
+// ✅ Move this part outside the function
+$nums = [1, 2, 3, 4, 5, 6, 7, 8];
+$target = 4;
+$result = TwoSum($nums, $target);
+
+// Print result
+print_r($result);
 ?>
